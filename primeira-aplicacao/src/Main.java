33df6443fe7fc6e83854c565e25fabd67dc85f79
Gui Ferreira <guifl_pereira@hotmail.com>
2024-09-12 16:21:58 -0300

    Programa do primeiro dia de estudos

---
+++ primeira-aplicacao/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Esse é o Screen Match");
+    System.out.println("Filme: Top Gun: Maverick");
+
+    int anoDeLancamento = 2002;
+
+    System.out.println("Ano de lançamento: " + anoDeLancamento);
+    boolean incluidoNoPlano = true;
+    double notaDoFilme = 8.1;
+
+    System.out.println("O filme está incluso no seu plano: " + incluidoNoPlano);
+    System.out.println("A nota do filme na plataforma é de: " + notaDoFilme);
+
+  }
+}
